ffb34f1d0a3560289a6e81343c3651967beabcbf
Morteza Nourani <mortizanourani@gmail.com>
2022-07-07 01:58:39 +0430

    Fix bug
    - In rules checking methods, the board property of sudoku class got
      used. It made bug for checking solutions of game. So we add an
      argument to these methods named board that send a copy of game current
      board to method.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,30 +1,30 @@
 public class Sudoku {
     public int[][] Board;
 
-    private boolean isInRow(int row, int value) {
+    private boolean isInRow(int[][] board, int row, int value) {
         for (int c = 0; c <= 8; c++) {
-            if (Board[row][c] == value) {
+            if (board[row][c] == value) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isInColumn(int column, int value) {
+    private boolean isInColumn(int[][] board, int column, int value) {
         for (int r = 0; r <= 8; r++) {
-            if (Board[r][column] == value) {
+            if (board[r][column] == value) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isInGroup(int row, int column, int value) {
+    private boolean isInGroup(int[][] board, int row, int column, int value) {
         int groupRow = (row / 3) * 3;
         int groupColumn = (column / 3) * 3;
         for (int r = groupRow; r <= groupRow + 3; r++) {
             for (int c = groupColumn; c <= groupColumn + 3; c++) {
-                if (Board[r][c] == value) {
+                if (board[r][c] == value) {
                     return true;
                 }
             }
