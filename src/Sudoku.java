f6bcef1c6905effef3e13448c227c96fc7ca9ffe
Morteza Nourani <mortizanourani@gmail.com>
2022-07-06 23:43:17 +0430

    Add Sudoku class
    - Sudoku class added. It will include methods for sudoku game rules.
    - Three main rules of sudoku game added to sudoku class.
    - The isInRow method check if the input value exists in specific row.
    - The isInColumn method check if the input value exists in specific
      column.
    - The isInGroup method check if the input value exists in specific 3x3
      group of table.
    - These 3 rules check if entered value is correct or not.

---
+++ src/Sudoku.java
@@ -0,0 +1,34 @@
+public class Sudoku {
+    public int[][] Board;
+
+    private boolean isInRow(int row, int value) {
+        for (int c = 0; c <= 8; c++) {
+            if (Board[row][c] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isInColumn(int column, int value) {
+        for (int r = 0; r <= 8; r++) {
+            if (Board[r][column] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isInGroup(int row, int column, int value) {
+        int groupRow = (row / 3) * 3;
+        int groupColumn = (column / 3) * 3;
+        for (int r = groupColumn; r <= groupColumn + 3; r++) {
+            for (int c = groupColumn; c <= groupColumn + 3; c++) {
+                if (Board[r][c] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
