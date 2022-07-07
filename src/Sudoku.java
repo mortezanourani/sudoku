bf5248281a807067da821e29e0a99fd3226e8bfe
Morteza Nourani <mortizanourani@gmail.com>
2022-07-07 14:03:48 +0430

    Update Sudoku class
    - Sudoku class updated to use Board property for generating sudoku
      table. So board argument of methods removed.
    - To create different sudoku tables, random choices added to hasSolution
      method in Sudoku class. It choose a random number and continue the
      backtracking algorithm to check if there is a solution.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,30 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Sudoku {
-    public int[][] Board;
+    public int[][] Board = new int[9][9];
 
-    private boolean isInRow(int[][] board, int row, int value) {
+    private boolean isInRow(int row, int value) {
         for (int c = 0; c <= 8; c++) {
-            if (board[row][c] == value) {
+            if (Board[row][c] == value) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isInColumn(int[][] board, int column, int value) {
+    private boolean isInColumn(int column, int value) {
         for (int r = 0; r <= 8; r++) {
-            if (board[r][column] == value) {
+            if (Board[r][column] == value) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isInGroup(int[][] board, int row, int column, int value) {
+    private boolean isInGroup(int row, int column, int value) {
         int groupRow = (row / 3) * 3;
         int groupColumn = (column / 3) * 3;
-        for (int r = groupRow; r <= groupRow + 3; r++) {
-            for (int c = groupColumn; c <= groupColumn + 3; c++) {
-                if (board[r][c] == value) {
+        for (int r = groupRow; r <= groupRow + 2; r++) {
+            for (int c = groupColumn; c <= groupColumn + 2; c++) {
+                if (Board[r][c] == value) {
                     return true;
                 }
             }
@@ -32,10 +35,10 @@ public class Sudoku {
         return false;
     }
     
-    private boolean isFilled(int[][] board) {
+    private boolean isFilled() {
         for (int r = 0; r <= 8; r++) {
             for (int c = 0; c <= 8; c++) {
-                if (board[r][c] == 0) {
+                if (Board[r][c] == 0) {
                     return false;
                 }
             }
@@ -43,26 +46,26 @@ public class Sudoku {
         return true;
     }
 
-    private boolean isWinner(int[][] board) {
+    private boolean isWinner() {
         int value;
         boolean isDuplicated;
         for (int r = 0; r <= 8; r++) {
             for (int c = 0; c <= 8; c++) {
-                value = board[r][c];
-                board[r][c] = 0;
-                isDuplicated = isInRow(board, r, value) || isInColumn(board, c, value) || isInGroup(board, r, c, value);
+                value = Board[r][c];
+                Board[r][c] = 0;
+                isDuplicated = isInRow(r, value) || isInColumn(c, value) || isInGroup(r, c, value);
                 if (isDuplicated) {
                     return false;
                 }
-                board[r][c] = value;
+                Board[r][c] = value;
             }
         }
         return true;
     }
 
-    private boolean hasSolution(int[][] board) {
-        if (isFilled(board)) {
-            if (isWinner(board)) {
+    private boolean hasSolution() {
+        if (isFilled()) {
+            if (isWinner()) {
                 return true;
             }
             return false;
@@ -71,21 +74,28 @@ public class Sudoku {
         boolean isDuplicated;
         for (int r = 0; r <= 8; r++) {
             for (int c = 0; c <= 8; c++) {
-                if (board[r][c] != 0) {
-                    continue;
-                } else {
-                    for (int v = 1; v <= 9; v++) {
-                        isDuplicated = isInRow(board, r, v) || isInColumn(board, c, v) || isInGroup(board, r, c, v);
-                        if (isDuplicated) {
-                            continue;
-                        } else {
-                            board[r][c] = v;
-                            if (hasSolution(board)) {
+                if (Board[r][c] == 0) {
+                    ArrayList<Integer> choices = new ArrayList<Integer>();
+                    Random rand = new Random();
+                    for (int e = 1; e <= 9; e++) {
+                        choices.add(e);
+                    }
+                    while (choices.size() > 0) {
+                        int randChoice = rand.nextInt(choices.size());
+                        int v = choices.get(randChoice);
+                        choices.remove(randChoice);
+                        isDuplicated = isInRow(r, v) || isInColumn(c, v) || isInGroup(r, c, v);
+                        if (!isDuplicated) {
+                            Board[r][c] = v;
+                            if (hasSolution()) {
                                 return true;
                             } else {
-                                board[r][c] = 0;
+                                Board[r][c] = 0;
                             }
                         }
+                    }
+                    if (Board[r][c] == 0) {
+                        return false;
                     }
                 }
             }
